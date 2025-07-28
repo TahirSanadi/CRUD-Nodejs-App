edff5df39a733c20d7dab14987491ccdf71d4ce3
TahirSanadi <tahirsanadi14@gmail.com>
2025-07-28 12:08:29 +0530

    added env

--- db/db.js
+++ db/db.js
@@ -1,10 +1,10 @@
-const mysql=require('mysql2');
+const mysql = require('mysql2');
+require('dotenv').config();
+const conn = mysql.createPool({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'root',
+    database: process.env.DB_NAME || 'pracproj'
+});
 
-const conn=mysql.createPool({
-    host:'localhost',
-    user:'root',
-    password:'root',
-    database:'pracproj'
-})
-
-module.exports=conn.promise();
+module.exports = conn.promise();
