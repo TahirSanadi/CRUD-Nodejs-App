666878bd1b93f536d0ecc3dc5cb8d0d077830281
TahirSanadi <tahirsanadi14@gmail.com>
2025-07-28 12:44:51 +0530

    used onlinedb

--- db/db.js
+++ db/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 const conn = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'root',
-    database: process.env.DB_NAME || 'pracproj'
+    host: process.env.DB_HOST || 'sql12.freesqldatabase.com',
+    user: process.env.DB_USER || 'sql12792315',
+    password: process.env.DB_PASSWORD || 'xJUXRy3mFa',
+    database: process.env.DB_NAME || 'sql12792315'
 });
 
 module.exports = conn.promise();
